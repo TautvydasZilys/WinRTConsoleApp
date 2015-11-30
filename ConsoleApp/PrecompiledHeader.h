68c1769f30e91e0b7d1615a713c4291514d2be09
Sunius <suniusx@gmail.com>
2015-11-30 20:04:47 +0200

    Empty app commit.

---
+++ ConsoleApp/PrecompiledHeader.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Windows.h>
+#include <wrl.h>
+
+#include <iostream>
---
+++ ConsoleApp/main.cpp
@@ -0,0 +1,6 @@
+#include "PrecompiledHeader.h"
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+{
+	return 0;
+}
